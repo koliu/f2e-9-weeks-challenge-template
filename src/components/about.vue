2836a944e0f3becc8d7c934490470ce87c7f249e
ko <ko.liu.learn@gmail.com>
2018-06-26 17:11:00 +0800

    adjust about.vue to use scss/pug

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,34 +1,37 @@
-<template>
-  <div id="about" @click="close">
-    <button class="btn-close" @click.stop="close">X</button>
-    <div class="menu"  @click.stop="() => {}">
-      <button class="menu-btn" :class="{ 'active' : show === 'about'}" @click.stop="show = 'about'">About</button>
-      <button class="menu-btn" :class="{ 'active' : show === 'specs'}" @click.stop="show = 'specs'">Specs</button>
-      <button class="menu-btn" :class="{ 'active' : show === 'specs-local'}" @click.stop="show = 'specs-local'">Specs(local)</button>
-    </div>
-    <div class="about" v-show="show === 'about'" @click.stop="() => {}">
-      <div class="about-item">
-        我是 KO<br>
-        一個 fullstack 導向的攻城獅<br>
-        這裡是攻打 F2E 的第三個座城堡 -- admin order<br>
-        目前攻打的目標： page-home<br>
-        採用的武器有<br>
-        - flexbox<br>
-        - scss<br>
-        - 樣式與排版分離<br>
-        - vue component<br>
-        - chart.js<br>
-        - webpack<br>
-      </div>
-      <div class="about-item" @click.stop="() => {}">
-        由於 F2E 開啟 蟲洞<br>
-        目前我方已被傳送到其他據點<br>
-        等待時機再度回來攻城……
-      </div>
-    </div>
-    <iframe class="specs" v-show="show === 'specs'" src="https://hexschool.github.io/THE_F2E_Design/week3-admin%20order/"></iframe>
-    
-    <iframe class="specs" v-show="show === 'specs-local'" src="../src/static/spec.html"></iframe>
+﻿<template lang="pug">
+  #about(@click="close")
+    button.btn-close(@click.stop="close") X
+    .menu(@click.stop="() => {}")
+      button.menu-btn(:class="{ 'active' : show === 'about'}" @click.stop="show = 'about'") About
+      button.menu-btn(:class="{ 'active' : show === 'specs'}" @click.stop="show = 'specs'") Specs
+      button.menu-btn(:class="{ 'active' : show === 'specs-local'}" @click.stop="show = 'specs-local'") Specs(local)
+    .about(v-show="show === 'about'" @click.stop="() => {}")
+      .about-item
+        i 我是 KO
+        br
+        i 一個 fullstack 導向的攻城獅
+        br
+        i 這裡是攻打 F2E 的第 3 座城堡 -- admin order
+        br
+        i 目前攻打的目標： page-home
+        br
+        i 採用的武器有
+        br
+        ul
+          li flexbox
+          li scss
+          li 樣式與排版分離
+          li vue component
+          li chart.js
+          li webpack
+      .about-item(@click.stop="() => {}")
+        i 由於 F2E 開啟 蟲洞
+        br
+        i 目前我方已被傳送到其他據點
+        br
+        i 等待時機再度回來攻城……
+    iframe.specs(v-if="show === 'specs'" src="https://hexschool.github.io/THE_F2E_Design/week3-admin%20order/")
+    iframe.specs(v-show="show === 'specs-local'" src="../src/static/spec.html")
   </div>
 </template>
 
@@ -49,7 +52,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 #about {
   align-items: center;
   border: 1px dotted #fff;
@@ -60,31 +63,32 @@ export default {
   height: 100%;
   width: 100%;
   z-index: 99999;
-}
 
-#about .menu {
-  display: flex;
-  height: 40px;
-}
-#about .menu-btn {
-  background: #000;
-  border: none;
-  color: #fff;
-  cursor: pointer;
-  font-size: 20px;
-  height: 100%;
-  padding: 5px 25px;
-}
+  .menu {
+    display: flex;
+    height: 40px;
 
-#about .menu-btn.active {
-  border-bottom: 4px solid #fff;
-  box-sizing: border-box;
-  cursor: default;
-}
+    .menu-btn {
+      background: #000;
+      border: none;
+      color: #fff;
+      cursor: pointer;
+      font-size: 20px;
+      height: 100%;
+      padding: 5px 25px;
 
-#about .menu-btn:hover:not(.active) {
-  background-color: rgba(255, 255, 255, 0.2);
-  cursor: pointer;
+      &.active {
+        border-bottom: 4px solid #fff;
+        box-sizing: border-box;
+        cursor: default;
+      }
+
+      &:hover:not(.active) {
+        background-color: rgba(255, 255, 255, 0.2);
+        cursor: pointer;
+      }
+    }
+  }
 }
 
 .btn-close {
@@ -95,13 +99,13 @@ export default {
   position: fixed;
   right: 15px;
   top: 15px;
-}
 
-.btn-close:hover {
-  border-color: #f5a623;
-  color: #f5a623;
-  cursor: pointer;
-  font-weight: bolder;
+  &:hover {
+    border-color: #f5a623;
+    color: #f5a623;
+    cursor: pointer;
+    font-weight: bolder;
+  }
 }
 
 .about {
@@ -112,15 +116,15 @@ export default {
   /* height: 90%; */
   margin-top: 24px;
   /* width: 90%; */
-}
 
-.about-item {
-  border-radius: 8px;
-  background-color: rgba(255, 255, 255, 0.3);
-  color: #f5a623;
-  line-height: 30px;
-  padding: 24px;
-  margin-bottom: 10px;
+  .about-item {
+    border-radius: 8px;
+    background-color: rgba(255, 255, 255, 0.3);
+    color: #f5a623;
+    line-height: 30px;
+    padding: 24px;
+    margin-bottom: 10px;
+  }
 }
 .specs {
   border: 1px dashed #f5a623;
